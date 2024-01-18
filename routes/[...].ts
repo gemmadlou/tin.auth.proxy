a335cf91b751d9965b6e464138c4968122733e80
Gemma Black <gblackuk@googlemail.com>
2024-01-18 21:49:33 +0000

    Get session cookie generated by clerk server

--- routes/[...].ts
+++ routes/[...].ts
@@ -2,5 +2,15 @@ export default eventHandler(async (event) => {
   let targetUrl = (process.env.PROXY_TARGET_URL ?? "http://localhost:5173/")
     .replace(/\/$/g, '') + event.path
 
-    return await proxyRequest(event, targetUrl)
+  let sessionCookie = getCookie(event, "__session");
+
+  let jwkUrl = process.env.JWK_URL
+  console.log({sessionCookie})
+
+  if (sessionCookie === undefined) {
+    setResponseStatus(event, 401)
+    return { error: "not signed in" };
+  }
+
+  return await proxyRequest(event, targetUrl)
 })
