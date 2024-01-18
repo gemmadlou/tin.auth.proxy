6b6bf8ea546d6ead566c0e6f60d2a8027172085b
Gemma Black <gblackuk@googlemail.com>
2024-01-18 22:08:07 +0000

    Decode session cookie JWT

--- routes/[...].ts
+++ routes/[...].ts
@@ -1,3 +1,7 @@
+import { ofetch } from "ofetch";
+import jwt from "jsonwebtoken";
+import jwksClient from "jwks-rsa";
+
 export default eventHandler(async (event) => {
   let targetUrl = (process.env.PROXY_TARGET_URL ?? "http://localhost:5173/")
     .replace(/\/$/g, '') + event.path
@@ -5,12 +9,28 @@ export default eventHandler(async (event) => {
   let sessionCookie = getCookie(event, "__session");
 
   let jwkUrl = process.env.JWK_URL
-  console.log({sessionCookie})
 
   if (sessionCookie === undefined) {
     setResponseStatus(event, 401)
     return { error: "not signed in" };
   }
 
-  return await proxyRequest(event, targetUrl)
+  try {
+    const client = jwksClient({
+      // rateLimit: true,
+      // jwksRequestsPerMinute: 10, // Default value
+      jwksUri: process.env.JWK_URL
+    });
+
+    let key = await client.getSigningKey()
+
+    let decoded = jwt.verify(sessionCookie, await key.getPublicKey())
+
+    console.log({ decoded })
+
+    return await proxyRequest(event, targetUrl)
+  } catch (error) {
+    setResponseStatus(event, 403)
+    return { error: "invalidToken" }
+  }
 })
