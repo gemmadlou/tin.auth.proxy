c08c8c3f9795ae3e2efc8eaae4b117457f3429fb
Gemma Black <gblackuk@googlemail.com>
2024-01-19 23:39:01 +0000

    Replace proxy server cookie lookup with middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,30 @@
+import jwksClient from "jwks-rsa";
+import jwt from "jsonwebtoken";
+
+export default defineEventHandler(async (event) => {
+    let sessionCookie = getCookie(event, "__session");
+
+    let isExcludedPath = (new RegExp(/.jpg|.png|.jpef|._nuxt|.svg|.css|node_modules/)).test(event.path)
+
+    if (sessionCookie && !isExcludedPath) {
+
+        try {
+            const client = jwksClient({
+                rateLimit: true,
+                jwksRequestsPerMinute: 5,
+                jwksUri: process.env.JWK_URL
+            });
+
+            let key = await client.getSigningKey()
+
+            event.context.auth = jwt.verify(sessionCookie, await key.getPublicKey())
+
+        } catch (error) {
+            console.log({ error }, process.env.AUTH_FE_URL, event.path, "expired")
+        }
+
+    } else if (isExcludedPath) {
+        event.context.skipAuth = true
+    }
+    
+})  
--- routes/[...].ts
+++ routes/[...].ts
@@ -3,46 +3,17 @@ import jwksClient from "jwks-rsa";
 
 export default eventHandler(async (event) => {
 
-  let isExcludedPath = (new RegExp(/.jpg|.png|.jpef|.+nuxt/)).test(event.path)
-
-  let sessionCookie = getCookie(event, "__session");
-
-  let jwkUrl = process.env.JWK_URL
-
-  if (sessionCookie === undefined) {
+  if (!event.context.auth && !event.context.skipAuth) {
     return await proxyRequest(event, process.env.AUTH_FE_URL.replace(/\/$/g, '') + event.path)
   }
 
   let targetUrl = (process.env.PROXY_TARGET_URL)
     .replace(/\/$/g, '') + event.path
 
-  if (!isExcludedPath) {
-    let decoded;
-
-    try {
-      const client = jwksClient({
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: process.env.JWK_URL
-      });
-
-      let key = await client.getSigningKey()
-
-      decoded = jwt.verify(sessionCookie, await key.getPublicKey())
-
-    } catch (error) {
-      console.log({ error }, process.env.AUTH_FE_URL)
-      return await proxyRequest(event, process.env.AUTH_FE_URL.replace(/\/$/g, '') + event.path)
+  return await proxyRequest(event, targetUrl, {
+    headers: {
+      user_id: event.context?.auth?.sub?.toString()
     }
-
-    return await proxyRequest(event, targetUrl, {
-      headers: {
-        user_id: decoded?.sub?.toString()
-      }
-    })
-
-  }
-
-  return await proxyRequest(event, targetUrl)
+  })
 
 })
