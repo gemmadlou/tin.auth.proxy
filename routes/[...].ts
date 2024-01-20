2e20b179283cb68bd1fc1698c546844675f60afa
Gemma Black <gblackuk@googlemail.com>
2024-01-20 22:37:07 +0000

    Handle session as a separate endpoint
    I was being super silly trying to serve to servers
    through the same path. Of course, having assets
    needed for the session was not being proxied correctly
    to the Auth UI. Therefore, the javascript that
    loaded the auth UI pages, just didn't work.

--- routes/[...].ts
+++ routes/[...].ts
@@ -3,9 +3,14 @@ import jwksClient from "jwks-rsa";
 
 export default eventHandler(async (event) => {
 
-  if (!event.context.auth) {
-    console.log('Auth required', event.context.auth)
+  if (event.path.startsWith("/session")) {
+    console.log("Go through to Auth UI")
     return await proxyRequest(event, process.env.AUTH_FE_URL.replace(/\/$/g, '') + event.path)
+  } 
+
+  if (!event.context.auth) {
+    console.log('Auth required', event.path)
+    return await sendRedirect(event, "/session");
   }
 
   let targetUrl = (process.env.PROXY_TARGET_URL)
